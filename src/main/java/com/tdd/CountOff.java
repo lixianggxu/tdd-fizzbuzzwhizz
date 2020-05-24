03e05e837d385b0fee87469c1d8bdee4ef11d36d
lixiang093214 <lixianggxu@163.com>
2020-05-24 20:55:55 +0800

    学生的序号为5倍数的场景；

--- src/main/java/com/tdd/CountOff.java
+++ src/main/java/com/tdd/CountOff.java
@@ -5,6 +5,9 @@ public class CountOff {
         if (i % 3 == 0) {
             return "Fizz";
         }
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
 
         return String.valueOf(i);
     }
--- src/test/java/com/tdd/CountOffTest.java
+++ src/test/java/com/tdd/CountOffTest.java
@@ -10,4 +10,10 @@ public class CountOffTest {
         CountOff countoff3 = new CountOff();
         assertThat("Fizz").isEqualTo(countoff3.Say(3));
     }
+
+    @Test
+    public void should_say_Buzz_when_count_off_given_student_NO_is_5() {
+        CountOff countoff = new CountOff();
+        assertThat("Buzz").isEqualTo(countoff.Say(5));
+    }
 }
